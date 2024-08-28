f2955d16137b2aa59d095de6fec2ca07c2959433
Mina Emad <menamanos@gmail.com>
2024-08-28 22:41:34 +0300

    validation schemas

---
+++ src/schemas/users.js
@@ -0,0 +1,48 @@
+import { passwordConfirmation } from "../custom-validators.js";
+import { body } from "express-validator";
+
+export const createSchema = {
+	username: {
+		notEmpty: { errorMessage: "Username can't be empty" },
+		isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
+	},
+	email: {
+		notEmpty: { errorMessage: "Email can't be empty" },
+		isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
+	},
+	password: {
+		notEmpty: { errorMessage: "Password can't be empty" },
+		isStrongPassword: { errorMessage: "Password has to be strong" },
+	},
+	password_confirm: {
+		notEmpty: { errorMessage: "Password Confirmation can't be empty" },
+		custom: {
+			options: passwordConfirmation,
+			errorMessage: "Password and Password Confirmation not equal",
+		},
+	},
+};
+
+export const patchSchema = {
+	username: {
+		optional: true,
+		isAlphanumeric: { errorMessage: "Username has to be Alphanumeric" },
+	},
+	email: {
+		optional: true,
+		isEmail: { errorMessage: "Email invalid, Email: john@doe.end" },
+	},
+	password: {
+		if: body("password_confirm").notEmpty(),
+		notEmpty: { errorMessage: "Password can't be empty" },
+		isStrongPassword: { errorMessage: "Password has to be strong" },
+	},
+	password_confirm: {
+		if: body("password").notEmpty(),
+		notEmpty: { errorMessage: "Password Confirmation can't be empty" },
+		custom: {
+			options: passwordConfirmation,
+			errorMessage: "Password and Password Confirmation not equal",
+		},
+	},
+};
