02f8b9db1767d38ba45b141ad208090d6507e13c
Mina Emad <menamanos@gmail.com>
2024-09-23 18:15:34 +0300

    log

--- src/strategies/local.js
+++ src/strategies/local.js
@@ -5,30 +5,31 @@ import { hashPassword } from "../utils/hashing.js";
 import { compareSync } from "bcrypt";
 
 export default passport.use(
-	new Strategy({ usernameField: "email" }, async (email, password, done) => {
-		const user = await User.findOne({ email: email });
+  new Strategy({ usernameField: "email" }, async (email, password, done) => {
+    const user = await User.findOne({ email: email });
 
-		try {
-			if (!user) throw new Error("User Not Found");
-			if (!compareSync(password, user.password))
-				throw new Error("Incorrect Password");
-		} catch (err) {
-			done(err, null);
-		}
-		done(null, user);
-	}),
+    try {
+      if (!user) throw new Error("User Not Found");
+      if (!compareSync(password, user.password))
+        throw new Error("Incorrect Password");
+    } catch (err) {
+      console.log(err);
+      done(err, null);
+    }
+    done(null, user);
+  }),
 );
 
 passport.serializeUser((user, done) => {
-	done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-	try {
-		const user = await User.findById(id);
-		if (!user) throw new Error("User Not Found");
-		done(null, user);
-	} catch (err) {
-		done(err, null);
-	}
+  try {
+    const user = await User.findById(id);
+    if (!user) throw new Error("User Not Found");
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
